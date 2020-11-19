ca064a91303b61dfc6fa2e8a70fcd48ea22d174a
calcarlson <calcarlson@gmail.com>
2020-11-18 19:57:34 -0600

    basic project structure

---
+++ iPad_Calendar_App/iPad_Calendar_App/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iPad_Calendar_App
+//
+//  Created by Cal Carlson on 11/18/20.
+//  Copyright © 2020 Cal Carlson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
